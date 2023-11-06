843fc59bcddf9d7db27f6f75c8ad0db9ce265ca9
smoothwarhead <simeont126@outlook.com>
2023-11-06 17:51:40 -0600

    latest commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 
-const BASE_URL = "https://localhost:7003";
+const BASE_URL = "https://app-kerryco-web-api-centralus-dev-001.azurewebsites.net/";
 const EXAMPLE_URL = "https://localhost:7079";
 
 
